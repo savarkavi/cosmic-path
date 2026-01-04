05a18c95c690b51068c7cfa5b3bcbcd0742c0646
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-01-04 16:40:20 +0530

    adjust some styles of hero-componets and add site logo

--- src/components/pages/home/hero/hero-content.tsx
+++ src/components/pages/home/hero/hero-content.tsx
@@ -6,9 +6,9 @@ const HeroContent = () => {
   return (
     <div className="z-10 h-dvh w-full">
       <div className="flex h-full w-full flex-col justify-center gap-8">
-        <div className="bg-accent-foreground w-fit rounded-full px-8 py-2 text-white capitalize">
+        {/* <div className="bg-accent-foreground w-fit rounded-full px-8 py-2 text-white capitalize">
           <p>Vedic mastery course now available</p>
-        </div>
+        </div> */}
         <div className="flex flex-col gap-8">
           <p className="font-italiana flex flex-col text-8xl">
             <span className="flex items-center gap-2">
@@ -18,7 +18,7 @@ const HeroContent = () => {
             <span>Guidance &</span>
             <span className="text-primary">Beyond</span>
           </p>
-          <p className="text-muted-foreground max-w-[600px] text-2xl font-light">
+          <p className="text-muted-foreground max-w-[600px] text-3xl font-light">
             Follow the path of the cosmos. Bring order to your chaos and
             illuminate the lives of those around you through ancient starlit
             wisdom.
--- src/components/pages/home/hero/index.tsx
+++ src/components/pages/home/hero/index.tsx
@@ -3,8 +3,8 @@ import HeroContent from "./hero-content";
 
 const HeroSection = () => {
   return (
-    <section className="flex h-dvh bg-radial-[at_0%_0%] from-[#FFEFBA] to-[#ffffff]">
-      <div className="mx-auto flex max-w-[1600px] items-center">
+    <section className="flex h-dvh bg-radial-[120%_100%_at_0%_0%] from-[#FFEFBA] from-30% to-[#ffffff]">
+      <div className="mx-auto flex max-w-[1500px] items-center gap-12 pt-12">
         <HeroImage />
         <HeroContent />
       </div>
