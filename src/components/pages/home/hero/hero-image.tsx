77c8beaa6331fbb3e875b0d1b3e5dcecae179f6a
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-01-07 08:11:38 +0530

    update hero-image width

--- src/components/pages/home/hero/hero-image.tsx
+++ src/components/pages/home/hero/hero-image.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const HeroImage = () => {
   return (
     <div className="w-full">
-      <div className="relative h-[700px] w-[750px]">
+      <div className="relative h-[700px] w-[730px]">
         <Image
           src="/guru-cropped.png"
           alt="hero-image"
