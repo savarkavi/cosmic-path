0b62bc0a3632f0dbe673226c7edc53119a95a236
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-01-07 08:13:10 +0530

    adjust max width of hero-section container

--- src/components/pages/home/hero/index.tsx
+++ src/components/pages/home/hero/index.tsx
@@ -4,7 +4,7 @@ import HeroContent from "./hero-content";
 const HeroSection = () => {
   return (
     <section className="flex h-dvh bg-radial-[120%_100%_at_0%_0%] from-[#FFEFBA] from-30% to-[#ffffff]">
-      <div className="mx-auto flex max-w-[1500px] items-center gap-12 pt-12">
+      <div className="mx-auto flex max-w-[1400px] items-center gap-4 pt-12">
         <HeroImage />
         <HeroContent />
       </div>
