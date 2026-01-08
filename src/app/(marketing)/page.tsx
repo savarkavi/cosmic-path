d9aa984eeaf527c5f83eed81bb1d9ff2e55d370e
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-01-08 11:11:04 +0530

    add AboutSection and TestimonialSection in Home page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,11 +1,16 @@
+import AboutSection from "@/components/pages/home/about";
 import FeaturedCoursesSection from "@/components/pages/home/featured-courses";
 import HeroSection from "@/components/pages/home/hero";
+import TestimonialSection from "@/components/pages/home/testimonial";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <HeroSection />
       <FeaturedCoursesSection />
+      <AboutSection />
+      <TestimonialSection />
+      <div className="h-screen"></div>
     </div>
   );
 }
