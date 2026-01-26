17a551a336dad2f89e0e620ab643a3e5816bd185
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-01-26 09:32:55 +0530

    add convex storage url as hostname in nextConfig file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "grand-horse-125.convex.cloud",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
