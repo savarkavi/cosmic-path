cc4adefd09ac02fe3578bd14dd8e13638a17e100
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-01-28 06:04:22 +0530

    made create course page a minimium 100vh height

--- src/app/admin/courses/create/page.tsx
+++ src/app/admin/courses/create/page.tsx
@@ -2,7 +2,7 @@ import { CreateCourseForm } from "@/components/forms/create-course";
 
 const CreateCoursePage = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
+    <div className="bg-accent flex min-h-screen w-full items-center justify-center py-12">
       <CreateCourseForm />
     </div>
   );
