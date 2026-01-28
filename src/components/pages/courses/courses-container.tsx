c70df61d461a11936bf4316f5bcddb2a66e7c07f
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-01-28 06:05:07 +0530

    feat: integrate API query for featured courses and add loading skeletons

--- src/components/pages/courses/courses-container.tsx
+++ src/components/pages/courses/courses-container.tsx
@@ -1,11 +1,29 @@
+"use client";
+
 import CourseCard from "@/components/shared/course-card";
-import { coursesData } from "@/lib/constants";
+import { useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const CoursesContainer = () => {
+  const data = useQuery(api.courses.getFeaturedCourses);
+
+  if (data === undefined)
+    return (
+      <div className="mt-16 grid grid-cols-3 gap-12">
+        {[1, 2, 3].map((item) => (
+          <Skeleton
+            key={item}
+            className="h-125 w-full rounded-xl bg-gray-200"
+          />
+        ))}{" "}
+      </div>
+    );
+
   return (
     <div className="mt-12">
       <div className="grid grid-cols-3 gap-12">
-        {coursesData.map((data) => (
+        {data.map((data) => (
           <CourseCard key={data.title} course={data} />
         ))}
       </div>
