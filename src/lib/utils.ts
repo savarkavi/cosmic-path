a1e29309befb46d753747a2747799c23ddc4bcca
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-01-28 06:03:16 +0530

    add utility function to calculate discounted price of the course

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function calculateDiscountedPrice(
+  price: number,
+  discountPercentage?: number,
+): number {
+  if (!discountPercentage || discountPercentage <= 0) {
+    return price;
+  }
+
+  if (discountPercentage >= 100) {
+    return 0;
+  }
+
+  const discountedPrice = price - (price * discountPercentage) / 100;
+
+  return Math.round(discountedPrice);
+}
