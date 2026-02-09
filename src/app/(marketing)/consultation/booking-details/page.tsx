9585ec3c1d91ef59eb1d8862883f556e15ed8cf5
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-02-09 06:41:08 +0530

    feat: integrate user data retrieval in BookingDetailsPage and pass user info to BookingDetailsForm

--- src/app/(marketing)/consultation/booking-details/page.tsx
+++ src/app/(marketing)/consultation/booking-details/page.tsx
@@ -1,10 +1,25 @@
+"use client";
+
 import { BookingDetailsForm } from "@/components/forms/booking-details";
+import { useQuery } from "convex/react";
+import { api } from "../../../../../convex/_generated/api";
+import { Loader2 } from "lucide-react";
 
 const BookingDetailsPage = () => {
+  const user = useQuery(api.users.getMe);
+
+  if (user === undefined || user === null) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Loader2 className="animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen pt-30">
       <div className="container mx-auto px-4">
-        <BookingDetailsForm />
+        <BookingDetailsForm user={user} />
       </div>
     </div>
   );
