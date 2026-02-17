ebf6c3f89b2c2ef298e259bdac0c5d638fdcd5b0
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-02-17 07:43:41 +0530

    feat: add discounted price display to course card and improve layout

--- src/components/shared/course-card.tsx
+++ src/components/shared/course-card.tsx
@@ -4,17 +4,22 @@ import { Clock, Users2 } from "lucide-react";
 import { Button } from "../ui/button";
 import { Doc } from "../../../convex/_generated/dataModel";
 import Link from "next/link";
-import { formatINR } from "@/lib/utils";
+import { formatINR, calculateDiscountedPrice } from "@/lib/utils";
 
 interface CourseCardProps {
   course: Doc<"courses">;
 }
 
 const CourseCard = ({ course }: CourseCardProps) => {
+  const discountedPrice = calculateDiscountedPrice(
+    course.price,
+    course.discount,
+  );
+
   return (
     <div className="bg-background h-full w-full rounded-md shadow-lg">
-      <Link href={`/courses/${course.slug}`}>
-        <div className="relative h-[200px] w-full rounded-t-md md:h-[300px]">
+      <Link href={`/courses/${course.slug}`} className="flex h-full flex-col">
+        <div className="relative h-[200px] w-full shrink-0 rounded-t-md md:h-[300px]">
           <Image
             src={course.imageUrl ? course.imageUrl : "/placeholder.png"}
             alt="course banner image"
@@ -22,13 +27,13 @@ const CourseCard = ({ course }: CourseCardProps) => {
             className="rounded-t-md object-cover"
           />
           <div className="from-accent absolute bottom-0 left-0 h-24 w-full bg-linear-to-t" />
+          <div className="absolute top-3 right-3 z-10 flex items-center gap-2">
+            <p className="bg-primary/90 rounded-full px-4 py-1 text-white shadow-xs backdrop-blur-sm">{`₹${formatINR(discountedPrice)}`}</p>
+          </div>
         </div>
-        <div className="mt-4 flex flex-col gap-2 p-4">
+        <div className="flex h-full flex-col justify-between gap-2 p-4">
           <div className="flex items-center justify-between">
-            <p className="line-clamp-1 text-lg uppercase md:text-2xl">
-              {course.title}
-            </p>
-            <p className="bg-primary rounded-full px-4 py-1 text-white">{`₹${formatINR(course.price)}`}</p>
+            <p className="text-lg uppercase md:text-2xl">{course.title}</p>
           </div>
           <p className="text-muted-foreground line-clamp-2">
             {course.description}
@@ -44,7 +49,7 @@ const CourseCard = ({ course }: CourseCardProps) => {
               <p>{course.duration}</p>
             </div>
           </div>
-          <Button className="bg-foreground mt-4 cursor-pointer py-6 text-lg text-white">
+          <Button className="bg-foreground mt-2 cursor-pointer py-6 text-lg text-white">
             Enroll now
           </Button>
         </div>
