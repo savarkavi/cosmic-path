97151a734739e31f5d526b98e184c94be771e351
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-04-06 14:36:15 +0530

    refactor CoursesPage: simplify header and container components by removing filter logic

--- src/app/(marketing)/courses/page.tsx
+++ src/app/(marketing)/courses/page.tsx
@@ -1,40 +1,16 @@
 "use client";
 
-import { useSearchParams, useRouter, usePathname } from "next/navigation";
-import { Suspense, useCallback } from "react";
+import { Suspense } from "react";
 import BundleBanner from "@/components/pages/courses/bundle-banner";
 import CoursesContainer from "@/components/pages/courses/courses-container";
-import Header, { TypeFilter } from "@/components/pages/courses/header";
+import Header from "@/components/pages/courses/header";
 
 const CoursesPageContent = () => {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const filterParam = searchParams.get("type");
-  const validFilters: TypeFilter[] = ["all", "astrology", "vastu", "tarot"];
-  const filter: TypeFilter = validFilters.includes(filterParam as TypeFilter)
-    ? (filterParam as TypeFilter)
-    : "all";
-
-  const setFilter = useCallback(
-    (newFilter: TypeFilter) => {
-      const params = new URLSearchParams(searchParams.toString());
-      if (newFilter === "all" || !newFilter) {
-        params.delete("type");
-      } else {
-        params.set("type", newFilter);
-      }
-      router.replace(`${pathname}?${params.toString()}`, { scroll: false });
-    },
-    [pathname, router, searchParams],
-  );
-
   return (
     <div className="mx-auto w-full max-w-300 px-4 pt-28 md:pt-46 xl:px-0">
-      <Header filter={filter} onFilterChange={setFilter} />
+      <Header />
       <BundleBanner />
-      <CoursesContainer filter={filter} />
+      <CoursesContainer />
     </div>
   );
 };
